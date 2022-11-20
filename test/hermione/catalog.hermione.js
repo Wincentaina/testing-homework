81a27405f7f1fed338bbabd1b9fb9601b5837ebc
Wincentaina <high.yn.kom@gmail.com>
2022-11-20 21:37:13 +0700

    add cart counter

--- test/hermione/catalog.hermione.js
+++ test/hermione/catalog.hermione.js
@@ -49,6 +49,57 @@ describe('Каталог:', async () => {
             assert(await prodBtn.isExisting(), "Нет кнопки")
         }
     })
+
+    it('уже добавлен в корзину', async function({browser}) {
+        await browser.url("/hw/store/catalog/" + bugId);
+
+        const catalogContainer = await browser.$(".Catalog > .row:nth-child(2)");
+
+        for (let i = 1; i <= 25; i++) {
+            await browser.url("/hw/store/catalog/" + bugId);
+            const item = await catalogContainer.$(`div:nth-child(${i})`)
+            await item.waitForExist()
+
+            const ProdItem = await catalogContainer.$('.ProductItem')
+            const card_body = ProdItem.$(".card-body");
+            
+            const link = card_body.$(".card-link");
+
+            await link.click()
+            const prodBTN = browser.$('.ProductDetails-AddToCart')
+            await prodBTN.click()
+            
+            const inCart = browser.$('.CartBadge')
+            await browser.url("/hw/store/catalog/" + bugId)
+            const inCatalog = browser.$('.CartBadge')
+
+            assert(await inCart.isExisting(), "Нет сообщения о добавлении в details")
+            assert(await inCatalog.isExisting(), "Нет сообщения о добавлении в каталоге")
+        }
+    })
    
+    it('добавлен в корзину, повторное нажатие кнопки "добавить в корзину" должно увеличивать его количество', async function({browser}) {
+        await browser.url("/hw/store/catalog/" + bugId);
+
+        const catalogContainer = await browser.$(".Catalog > .row:nth-child(2)");
+
+        const ProdItem = await catalogContainer.$('.ProductItem')
+        const card_body = ProdItem.$(".card-body");
+        
+        const link = card_body.$(".card-link");
+        await link.click()
+        const prodBTN = browser.$('.ProductDetails-AddToCart')
+        await prodBTN.click()
+        await prodBTN.click()
+        
+        await browser.url("/hw/store/cart/" + bugId);
+
+        const counter = await browser.$('.Cart-Count')
+        const val = await counter.getText()
+        
+        assert(await val == '2' ? true: false, "Счетчик в корзине не обновляется")
+    })
+
 })
+
 
